5407139326a513de3d42acf1fe5545221145bcbc
Vinicius <viniciusmul98@gmail.com>
2025-04-20 13:51:30 -0300

    init

---
+++ exec.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+echo Hello $USER!
+echo Today is $(date)
+
+node index.js
---
+++ index.js
@@ -0,0 +1,29 @@
+const https = require("https");
+const options = {
+ hostname: "jsonplaceholder.typicode.com",
+ path: "/posts",
+ method: "GET"
+}
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    try {
+      const jsonData = JSON.parse(data);
+      console.log(jsonData);
+    } catch (err) {
+      console.error('Error parsing JSON:', err);
+    }
+  });
+});
+req.on('error', (e) => {
+  console.error(`Problem with request: ${e.message}`);
+});
+req.end();
+
+const sumNums = (a, b) => a + b;
+const welcome = (name) => `Welcome to the app ${name}`;
+console.log(welcome("vinicius"));
+console.log(sumNums(1,1));
